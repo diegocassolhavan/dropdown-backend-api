ba01dabfd1fbac5313e63163d78d335c6ee8b0e0
Diego Cassol <Diego.cassol@havan.com.br>
2026-01-11 16:30:08 -0300

    Remove old app folder to fix deployment

--- app/services/__init__.py
+++
@@ -1,6 +0,0 @@
-# Services module
-from .number_service import NumberService
-from .letter_service import LetterService
-from .selection_service import SelectionService
-
-__all__ = ["NumberService", "LetterService", "SelectionService"]
